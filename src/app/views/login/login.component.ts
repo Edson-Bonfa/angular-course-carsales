ae74aaf71fadb3bb46e21dcd6729703e8c7dd002
Edson-Bonfa <edsonbonfa@gmail.com>
2026-05-12 16:02:15 -0300

    Criando o dashboard

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { RequestLogin } from 'src/app/resources/models/RequestLogin';
 import { AlertService } from 'src/app/resources/services/alert.service';
 import { LoginService } from 'src/app/resources/services/login.service';
@@ -13,7 +14,8 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private loginService: LoginService,
-    private alertService: AlertService
+    private alertService: AlertService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -23,7 +25,8 @@ export class LoginComponent implements OnInit {
   public doLogin(): void {
     this.loginService.doLogin(this.requestLogin).subscribe(
       (data) => {
-        this.alertService.info('Funcionalidade ainda não implementada!');
+        //this.alertService.info('Funcionalidade ainda não implementada!');
+        this.router.navigate(['dashboard']);
       },
       (httpError) => {
         this.alertService.error(httpError.error.message);
